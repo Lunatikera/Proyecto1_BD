596fcbefe0a9c5783a1d194221de7d0cb3d13f0d
CriCrin03 <master_shotguns45@hotmail.com>
2024-06-06 18:17:11 -0700

    Proyecto con maven creado

---
+++ Cinepolis_216661_233537_240005/src/main/java/Cinepolis/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Cinepolis;
+
+/**
+ *
+ * @author Chris
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
