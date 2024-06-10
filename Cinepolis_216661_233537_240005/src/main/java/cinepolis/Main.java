f063d1429c6ca810baa6802c5f00be858f7e7cd9
Lunatikera <carlitosjuven@gmail.com>
2024-06-10 04:01:28 -0600

    push
    nuevas entidades y detalle peliculas

--- Cinepolis_216661_233537_240005/src/main/java/cinepolis/Main.java
+++ Cinepolis_216661_233537_240005/src/main/java/cinepolis/Main.java
@@ -3,13 +3,17 @@
  */
 package cinepolis;
 
-import forms.FrmCatalogoClientes;
 import forms.FrmInicio;
 import negocio.ClienteNegocio;
 import negocio.IClienteNegocio;
+import negocio.IPeliculaNegocio;
+import negocio.PeliculaNegocio;
 import persistencia.ClienteDAO;
 import persistencia.ConexionBD;
 import persistencia.IClienteDAO;
+import persistencia.IConexionBD;
+import persistencia.IPeliculaDAO;
+import persistencia.PeliculaDAO;
 
 /**
  *
@@ -20,10 +24,10 @@ public class Main {
     public static void main(String[] args) {
         IConexionBD conexionBD = new ConexionBD();
         IClienteDAO clientesDAO = new ClienteDAO(conexionBD);
-
+        IPeliculaDAO peliculaDAO= new PeliculaDAO(conexionBD);
         IClienteNegocio clienteNeg = new ClienteNegocio(clientesDAO);
-
-        FrmInicio frmCrud = new FrmInicio(clienteNeg);
+        IPeliculaNegocio peliculaNeg= new PeliculaNegocio(peliculaDAO);
+        FrmInicio frmCrud = new FrmInicio(clienteNeg, peliculaNeg);
         frmCrud.show();
 
     }
