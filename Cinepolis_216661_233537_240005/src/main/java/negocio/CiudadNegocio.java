370be7b25b5507a16da07b8ddb356d907467c467
Lunatikera <carlitosjuven@gmail.com>
2024-06-10 05:43:14 -0600

    push
    ciudad y pais

--- Cinepolis_216661_233537_240005/src/main/java/negocio/CiudadNegocio.java
+++ Cinepolis_216661_233537_240005/src/main/java/negocio/CiudadNegocio.java
@@ -4,10 +4,46 @@
  */
 package negocio;
 
+import dtos.CiudadDTO;
+import entidades.Ciudad;
+import java.util.ArrayList;
+import java.util.List;
+import persistencia.ICiudadDAO;
+import persistencia.PersistenciaException;
+
 /**
  *
  * @author Usuario
  */
-public class CiudadNegocio {
+public class CiudadNegocio implements ICiudadNegocio {
+     private ICiudadDAO ciudadDAO;
+
+    public CiudadNegocio(ICiudadDAO ciudadDAO) {
+        this.ciudadDAO = ciudadDAO;
+    }
     
+    public List<CiudadDTO> listaCiudades(String pais) throws NegocioException {
+        try {
+            // Obtener el ID del país usando el nombre del país
+
+            // Obtener la lista de ciudades para el país especificado
+            List<Ciudad> listaCiudades = ciudadDAO.listaCiudades(pais);
+
+            // Convertir la lista de Ciudad a CiudadDTO
+            List<CiudadDTO> listaCiudadesDTO = new ArrayList<>();
+            for (Ciudad ciudad : listaCiudades) {
+                listaCiudadesDTO.add(convertirACiudadDTO(ciudad));
+            }
+            return listaCiudadesDTO;
+
+        } catch (PersistenciaException e) {
+            throw new NegocioException("Error al obtener la lista de ciudades desde la capa de negocio", e);
+        }
+    }
+
+    // Método privado para convertir Ciudad a CiudadDTO
+    private CiudadDTO convertirACiudadDTO(Ciudad ciudad) {
+        return new CiudadDTO(ciudad.getId() , ciudad.getNombre());
+    }
+
 }
